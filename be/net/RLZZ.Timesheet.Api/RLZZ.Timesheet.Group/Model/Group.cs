44c04bf0bf68d42c798d16be2e8d03acfedcc065
DEVCOMP\erymn <erymnur@gmail.com>
2026-04-20 17:23:21 +0700

    Initiate model

--- be/net/RLZZ.Timesheet.Api/RLZZ.Timesheet.Group/Model/Group.cs
+++ be/net/RLZZ.Timesheet.Api/RLZZ.Timesheet.Group/Model/Group.cs
@@ -2,5 +2,14 @@ namespace RLZZ.Timesheet.Group.Model;
 
 public class Group
 {
-    
+    public int Id { get; set; }
+    public required string GroupId { get; set; }
+    public required string Name { get; set; }
+
+    public bool IsActive { get; set; } = true;
+    public bool IsDeleted { get; set; } = false;
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+    public required string CreatedBy { get; set; } = "system";
+    public DateTime ModifiedDate { get; set; } = DateTime.Now;
+    public required string ModifiedBy { get; set; } = "system";
 }
